bf934e408b77a84cce3606566c4dd8a8bca8cad3
Ayako Kurita <107830445+kurigohan0103@users.noreply.github.com>
2024-03-05 15:05:58 +0900

    Update MyPage.php

--- MyPage.php
+++ MyPage.php
@@ -29,25 +29,58 @@ $photo_url = $userInfo['photo_url'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./MyPage.css"/>
     <title>MyPage</title>
 </head>
 <body>
-    <h1>MyPage</h1>
+<div class="navigation">
+      <ul>
+        <li class="list active">
+          <a href="showData.php">
+            <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+            <span class="title">HOME</span>
+          </a>
+        </li>
+        <li class="list">
+          <a href="MyPage.php">
+            <span class="icon"><ion-icon name="person-circle-outline"></ion-icon></span>
+            <span class="title">PLOFILE</span>
+          </a>
+        </li>
+        <li class="list">
+          <a href="#">
+            <span class="icon"
+              ><ion-icon name="log-out-outline"></ion-icon
+            ></span>
+            <span class="title">SIGNOUT</span>
+          </a>
+        </li>
+      </ul>
+    </div>
 
-    <p>漢字名前:<?php echo $kanji_name; ?></p>
-    <p>ローマ字名前:<?php echo $$romaji_name; ?></p>
-    <p>所属:<?php echo $affiliation; ?></p>
-    <p>役職:<?php echo $$position; ?></p>
-    <p>住所:<?php echo $company_address; ?></p>
-    <p>電話番号:<?php echo $phon_number; ?></p>
-    <p>メールアドレス:<?php echo $email_address; ?></p>
-    <p>写真URL:<?php echo $photo_url; ?></p>
-    
+    <div class="content">
+        <div class="namecard">
+            <p class="name"><?php echo $kanji_name; ?></p>
+            <span class="phonetic"><?php echo $romaji_name; ?></span>
 
-    <button id="redirectButton1" type="button">編集</button>
-    <p></p>
-    <button id="redirectButton2" type="button">QRコード</button>
-    <button id="redirectButton3" type="button">カメラ</button>
+            <div class="contact">
+                
+            </div>
+        </div>
+        
+        <button class="button1" id="redirectButton1" type="button">Edit</button>
+
+        <div class="button2">
+            <button id="redirectButton2" type="button">
+                <ion-icon type="button" name="qr-code-outline"></ion-icon>
+            </button>
+
+            <button id="redirectButton3" type="button">
+                <ion-icon name="camera-outline"></ion-icon>
+            </button>
+        </div>
+        
+    </div>
 
     <script>
         document.addEventListener("DOMContentLoaded", function() {
@@ -73,5 +106,33 @@ $photo_url = $userInfo['photo_url'];
             });
         });
     </script>
+
+
+    <!-- サイドバーのJS -->
+    <script>
+      const list = document.querySelectorAll(".list");
+      console.log(list);
+      function activeLink() {
+        list.forEach((item) =>
+          // console.log(item);
+          item.classList.remove("active")
+        );
+        this.classList.add("active");
+      }
+
+      list.forEach((item) => {
+        item.addEventListener("click", activeLink);
+      });
+    </script>
+
+    <!-- アイコンの引用元 -->
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
 </body>
 </html>
